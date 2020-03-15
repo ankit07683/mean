be6090ad5694e1a919d4cdf340385a0be532db10
ankit jain <ankit07683@gmail.com>
2020-03-15 23:30:39 +0530

    fixed api url

--- src/app/auth-service.ts
+++ src/app/auth-service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiURI = 'http://localhost:3000/api/';
+  private apiURI = 'https://ak-mean.herokuapp.com/api/';
 
   response ='';
   error= '';
